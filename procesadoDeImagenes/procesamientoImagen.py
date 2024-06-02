0eb580c26352bf8e7e3fe2e25281da2ef1f3d42f
Belisk <blackbelisk@gmail.com>
2024-06-02 13:36:04 -0300

    Por favor Dios ten piedad

--- procesadoDeImagenes/procesamientoImagen.py
+++ procesadoDeImagenes/procesamientoImagen.py
@@ -7,33 +7,34 @@ def procesarATexto(AUTH, image_path):
     bw_image = grayscale_image.point(lambda x: 0 if x < threshold else 255, '1')
     texto_original = pytesseract.image_to_string(bw_image)
  
-    if(AUTH.lower() not in texto_original.lower()):
-        angulo = 0
-        k = 0
-        rotar = True
-        while (rotar) and (k < 20):
-            imagen_rotada = bw_image.rotate(angulo, expand = True)
-            texto_original = pytesseract.image_to_string(imagen_rotada)
+    # if(AUTH.lower() not in texto_original.lower()):
+    #     angulo = 0
+    #     k = 0
+    #     rotar = True
+    #     while (rotar) and (k < 20):
+    #         imagen_rotada = bw_image.rotate(angulo, expand = True)
+    #         texto_original = pytesseract.image_to_string(imagen_rotada)
 
-            if AUTH.lower() in texto_original.lower():
-                rotar = False
-            else:
-                k+=1
-                angulo+=5
+    #         if AUTH.lower() in texto_original.lower():
+    #             rotar = False
+    #         else:
+    #             k+=1
+    #             angulo+=5
 
-        if k >= 360:
-            return False
+    #     if k >= 360:
+    #         return False
 
     # Procesado normal
     texto_original = [line for line in texto_original.split('\n') if line.strip()]
+    result = ''
+    for value in texto_original:
+        if AUTH.lower() == value.lower():
+            result = texto_original[texto_original.index(AUTH) + 1]
 
-    for idx, value in enumerate(texto_original):
-        if AUTH.lower() in value.lower():
-            result = texto_original[idx+1]
     result = result.split(' ')
     result = ''.join(filter(str.isdigit, result))
     return result
 
-imagen = 'intento.png'
-nom = "instagram"
-print(procesarATexto(nom, imagen))
+# imagen = 'intentoDificil.jpg'
+# nom = "OSE"
+# print(procesarATexto(nom, imagen))
