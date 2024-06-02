1a40810a7fee9d27d9eca94977b41d7f0d630b69
Belisk <blackbelisk@gmail.com>
2024-06-02 12:47:05 -0300

    s

--- procesadoDeImagenes/procesamientoImagen.py
+++ procesadoDeImagenes/procesamientoImagen.py
@@ -1,29 +1,41 @@
 import pytesseract
 from PIL import Image
-import re
 
-DEFAULT_AUTH = "facebook"
-image_path = 'procesadoDeImagenes/recorte.png'
-image = Image.open(image_path)
-#image.show()
-grayscale_image = image.convert('L')
-threshold = 140
-bw_image = grayscale_image.point(lambda x: 0 if x < threshold else 255, '1')
-#bw_image.show()
-#custom_config = r'--oem 3 --psm 6 -c tessedit_char_whitelist=0123456789'
-#text = pytesseract.image_to_string(bw_image, config = custom_config)
-text = pytesseract.image_to_string(bw_image)
-text = [line for line in text.split('\n') if line.strip()]
+def procesarATexto(AUTH, image_path):
+    image = Image.open(image_path)
+    grayscale_image = image.convert('L')
+    threshold = 140
+    bw_image = grayscale_image.point(lambda x: 0 if x < threshold else 255, '1')
+    texto_original = pytesseract.image_to_string(bw_image)
 
-for idx, value in enumerate(text):
-    if DEFAULT_AUTH in value.lower():
-        result = text[idx+1]
+    
+    if(AUTH.lower() not in texto_original.lower()):
+        angulo = 0
+        k = 0
+        rotar = True
+        while (rotar) and (k < 360):
+            imagen_rotada = bw_image.rotate(angulo, expand = True)
+            texto_original = pytesseract.image_to_string(imagen_rotada)
 
-result = result.split(' ')
-result = ''.join(result)
-print(result)
-#numbers = ''.join(filter(str.isdigit, text))
+            if AUTH.lower() in texto_original.lower():
+                rotar = False
+            else:
+                k+=1
+                angulo+=1
 
-#output_file_path = 'numeros.txt'
-#with open(output_file_path, 'w') as file:
-#    file.write(numbers)
+        if k >= 360:
+            return False
+
+    # Procesado normal
+    texto_original = [line for line in texto_original.split('\n') if line.strip()]
+
+    for idx, value in enumerate(texto_original):
+        if AUTH.lower() in value.lower():
+            result = texto_original[idx+1]
+    result = result.split(' ')
+    result = ''.join(filter(str.isdigit, result))
+    return result
+
+imagen = 'otroIntento.jpg'
+nom = "OSE"
+print(procesarATexto(nom, imagen))
